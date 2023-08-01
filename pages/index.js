1a5591207d2b3cb70ba7b9831490027f11c94ae1
Tamura <tamurafelipe@gmail.com>
2023-08-01 11:46:41 -0300

    corrigindo created_at

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
+  
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
@@ -28,7 +29,7 @@ export default function Index({ posts, globalData }) {
                 href={`/posts/${post.id}`}
               >
                 <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.created_ate && (
+                  {post.created_at && (
                     <p className="uppercase mb-3 font-bold opacity-60">
                       {post.created_at}
                     </p>
@@ -39,6 +40,11 @@ export default function Index({ posts, globalData }) {
                       {post.description}
                     </p>
                   )}
+                  {/* {post.body && (
+                    <p className="mt-3 text-lg opacity-60">
+                      {post.body}
+                    </p>
+                  )} */}
                   <ArrowIcon className="mt-4" />
                 </a>
               </Link>
@@ -62,7 +68,6 @@ export default function Index({ posts, globalData }) {
 export async function getServerSideProps() {
   const posts = await getPosts();
   const globalData = getGlobalData()
-
 
   return { props: { posts, globalData } };
 }
