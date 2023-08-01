39ce8822a3635fd927be2f5c6bdb9525c12900fb
Tamura <tamurafelipe@gmail.com>
2023-08-01 11:47:13 -0300

    ajustes de baseURL, apikey e authorization

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://tqwrxrbrmraszcgyrsnc.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxd3J4cmJybXJhc3pjZ3lyc25jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDg5NTE2NCwiZXhwIjoyMDA2NDcxMTY0fQ.-SmXfSb3rYVRNu-a9RQ77_Dhxe5Sv_3fBDBcs7FalzU",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxd3J4cmJybXJhc3pjZ3lyc25jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MDg5NTE2NCwiZXhwIjoyMDA2NDcxMTY0fQ.-SmXfSb3rYVRNu-a9RQ77_Dhxe5Sv_3fBDBcs7FalzU"
     }
 })
